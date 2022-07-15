8953e6abaae04355f6dbbf49a070fe499eff7523
ADMIN <ADMIN@DESKTOP-VB164NU>
2022-07-15 18:02:48 +0530

    upated contactt

--- src/test/java/production/ContactModuleTest.java
+++ src/test/java/production/ContactModuleTest.java
@@ -8,15 +8,16 @@ public void createContactTest() {
 	System.out.println("createContactTest executed");
 	System.out.println("Create Contact Test module created by Pavithra ");
 	System.out.println("createOrganization Test module created by Pavithra Latest Model");
+	System.out.println("createOrganization Test module created by Pavithra Latest Model");
 }
 @Test
 public void createOrganizationTest() {
 	System.out.println("createOrganizationTest executed");
 	System.out.println("createOrganization Test module created by Pavithra");
-<<<<<<< HEAD
+
 	System.out.println("createOrganization Test module created by Pavithra Latest Model");
-=======
+
 	System.out.println(" Happy birthday to me ");
->>>>>>> branch 'master' of https://github.com/pavy114/TestYantra36B1Pavithra.git
+//>>>>>>> branch 'master' of https://github.com/pavy114/TestYantra36B1Pavithra.git
 }
 }
