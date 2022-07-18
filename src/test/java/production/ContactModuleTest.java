79af8ebe7cbed15b4898012f851f1a596b6cbe5c
ADMIN <ADMIN@DESKTOP-VB164NU>
2022-07-18 10:47:16 +0530

    Commit on 18-7-2022

--- src/test/java/production/ContactModuleTest.java
+++ src/test/java/production/ContactModuleTest.java
@@ -2,6 +2,9 @@ package production;
 
 import org.testng.annotations.Test;
 
+
+
+
 public class ContactModuleTest {
 @Test
 public void createContactTest() {
@@ -13,6 +16,19 @@ public void createContactTest() {
 //=======
 	System.out.println("createOrganization Test module created by Pavithra Latest Model2");
 //>>>>>>> branch 'master' of https://github.com/pavy114/TestYantra36B1Pavithra.git
+}
+
+
+public class ContactModule99999999Test {
+@Test
+public void createContactTest() {
+	System.out.println("createContactTest executed");
+	System.out.println("Create Contact Test module created by Pavithra ");
+	System.out.println("createOrganization Test module created by Pavithra Latest Model");
+//<<<<<<< HEAD
+	System.out.println("createOrganization Test module created by Pavithra Latest Model");
+//=======
+	System.out.println("createOrganization Test module created by Pavithra Latest Model2");
 }
 @Test
 public void createOrganizationTest() {
